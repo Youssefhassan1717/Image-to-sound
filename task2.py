aad1fe525e6ff57538cb65c18a3d26ad34cb600a
Youssef Abdelmaksoud <106936483+Youssefhassan1717@users.noreply.github.com>
2023-09-18 16:34:36 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,30 @@
+#import modules
+
+from PIL import Image
+from gtts import gTTS
+import pytesseract
+import os
+
+from new import image_to_sound
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+def convert_to_sound(image):
+
+    try:
+        open_image = Image.open(image)
+        text = pytesseract.image_to_string(open_image)
+        text_file = " ".join(text.split("\n"))
+        print(text_file)
+        sound = gTTS(text_file, lang="en")
+        sound.save("sound.mp3")
+        os.system("sound.mp3")
+        return True
+
+    except Exception as bug:
+        print("The error while executing the code\n", bug)
+        return
+
+if __name__ == "__main__":
+    image_to_sound("imagetext.jpg")
+    input()
